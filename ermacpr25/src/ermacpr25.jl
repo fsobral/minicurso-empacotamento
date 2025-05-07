e8988ca78df4655599f1e8d6532beb5df62288a3
Francisco N. C. Sobral <fsobral@uem.br>
2025-05-07 02:32:09 -0300

    Finish part 2.

--- ermacpr25/src/ermacpr25.jl
+++ ermacpr25/src/ermacpr25.jl
@@ -216,13 +216,27 @@ Plota a solução do problema de corte e empacotamento.
     - 'x': solução encontrada pelo solver no formato [xC, yC, xP, yP, θP].
 
 """
-function plota_solucao(lista_raios_circ, lista_vertice_pol, L, W, x)
+function plota_solucao(lista_raios_circ, lista_vertice_pol, L, W, xP, yP, theta_P, xL, yL, alpha_L, xC, yC)
+
+    xP_values = value.(xP)
+    yP_values = value.(yP)
+    theta_P_values = value.(theta_P)
+    xL_values = value.(xL)
+    yL_values = value.(yL)
+    alpha_L_values = value.(alpha_L)
+    xC_values = value.(xC)
+    yC_values = value.(yC)
 
     nc = size(lista_raios_circ)[1]
     np = length(lista_vertice_pol)
 
     # Plota o envelope retangular.
     fig = plot(caixa(L, W), fillcolor = :white, legend = false, aspect_ratio = :equal)
+
+    # Constroi lista das solucoes
+    z = [ [[i, j] for (i, j) in zip(xC_values, yC_values)]; 
+      [[i, j, k] for (i, j, k) in zip(xP_values, yP_values, theta_P_values)] ]
+    x = vcat(z...)
 
     # Plota os círculos.
     for i = 1:nc
@@ -278,5 +292,130 @@ function desenha_solucao_qc(ℓ, xQ, yQ, θ, R)
     end
 
     return fig
+
+end
+
+"""
+    le_arquivo(arquivo_csv)
+
+Le um aquivo .csv separado por '\t' com o seguinte formato
+  - Cada linha ou tem um raio, um polígono convexo ou um vazio
+  - Um polígono irregular é definido por uma sequência de linhas de polígonos convexos seguida de um vazio
+  - Cada coluna ou tem apenas o raio ou então tem as coordenadas de um vértice no formato `(xx.xxx, xx.xxx)`
+  - As colunas são separadas por tabulações (`\t`)
+"""
+function le_arquivo(arquivo_csv)
+
+    raios = []
+
+    P = []
+
+    rx = r"\((.*),(.*)\)"
+
+    open(arquivo_csv, "r") do fp
+
+        Pi = []
+
+        while !eof(fp)
+
+            linha = strip(readline(fp))
+
+            # Se encontrou uma linha em branco apos ler varios poligonos, então finaliza o poligono
+            if (strip(linha) == "") 
+                
+                (!isempty(Pi)) && append!(P, [Pi])
+
+                Pi = []
+
+            else
+
+                v = split(linha, '\t')
+
+                if length(v) == 1
+
+                    append!(raios, parse(Float64, v[1]))
+
+                else
+
+                    vs = [[parse(Float64, i) for i in m.captures] for m in [match(rx, s) for s in v]]
+                    append!(Pi, [hcat(vs...)'])
+
+                end
+
+            end
+        end
+
+        (!isempty(Pi)) && append!(P, [Pi])
+
+    end
+
+    return raios, P
+
+end
+
+"""
+    resolve_o_problema(W, L, arquivo_csv, tempo=30.0)
+
+Recebe as dimensões do retângulo e o arquivo `.csv` no formato exigido pela função `le_arquivo`. Devolve um desenho da solução.
+"""
+function resolve_o_problema(W, L, arquivo_csv, tempo=30.0)
+
+    raios, lista_poligonos = le_arquivo(arquivo_csv)
+
+    println("Leu o problema.")
+
+    lista_rotacionada =translacao_poligono(lista_poligonos)
+    rt = reta(lista_rotacionada)
+    rt = reta(lista_rotacionada)
+    nr = length(rt)
+    nc = length(circ_r)
+
+    #MODELO
+    model = Model(Ipopt.Optimizer)
+        
+        # Variáveis das coordenadas de referência
+        @variable(model, xC[1:nc], start = W * rand())
+        @variable(model, yC[1:nc], start = L * rand())
+        @variable(model, xP[1:nr], start = W * rand())
+        @variable(model, yP[1:nr], start = L * rand())
+        @variable(model, theta_P[1:nr], start = 2 * π * rand())
+
+        # Componentes da reta para comparação de polígonos
+        @variable(model, yL[i = 1:nr-1, h = 1:length(rt[i]), n = 1:sum(length(rt[j]) for j = i+1:nr)], start = rand())
+        @variable(model, xL[i = 1:nr-1, h = 1:length(rt[i]), n = 1:sum(length(rt[j]) for j = i+1:nr)], start=rand())
+        @variable(model, alpha_L[i = 1:nr-1, h = 1:length(rt[i]), n = 1:sum(length(rt[j]) for j = i+1:nr)], start=rand())
+
+        # Componentes da reta para o delta
+        @variable(model, yL_circ[i = 1:nr, h = 1:length(rt[i]), n = 1:nc], start = rand())
+        @variable(model, xL_circ[i = 1:nr, h = 1:length(rt[i]), n = 1:nc], start=rand())
+        @variable(model, alpha_L_circ[i = 1:nr, h = 1:length(rt[i]), n =1:nc], start=rand())
+        
+        nlex, nley = cosseno_model_teste!(model, lista_rotacionada, theta_P, xP, yP)
+        nlerc, nlerd = cdij!(model, rt, alpha_L, xL, yL)
+        nlerc_circ, nlerd_circ = cdij_circ!(model, rt, alpha_L_circ, xL_circ, yL_circ, circ_r)
+        
+        # Restrições de contenção
+        contcir!(model, circ_r, xC, yC, L, W)
+        nsobrecirc!(model, xC, yC, circ_r)
+        constraints!(model, lista_rotacionada, nlex, nley, nlerc, nlerd, L, W)  
+        
+        # Restrições de não sobreposição
+        n_sobreposicao!(model, lista_rotacionada, rt, nlex, nley, nlerc, nlerd)
+        delta!(model, lista_rotacionada, xC, yC, nlerc_circ, nlerd_circ, circ_r)
+        last_re!(model, lista_rotacionada, xC, yC, nlerc_circ, nlerd_circ, nlex, nley)
+        
+        @objective(model, Min, 0)
+        
+        println("Construiu o problema.")
+
+    # Chama o otimizador Ipopt:
+
+    set_attribute(model, "max_cpu_time", Float64(tempo))
+
+    set_attribute(model, "max_iter", 10000)
+
+    optimize!(model)
+
+    plota_solucao(circ_r,lista_rotacionada,L, W, xP, yP, theta_P, xL, yL, alpha_L, xC, yC)
 
 end
